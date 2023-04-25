19d00fd565208a8b7bfcc12561d34984b1137866
javadoterr <javadoterr@gmail.com>
2023-04-25 17:39:45 +0530

    method in UserRepository, CustomUserdetails, UserDetailsServiceImpl added

--- smartcontactmanager/src/main/java/com/javadoterr/api/dao/UserRepositoy.java
+++ smartcontactmanager/src/main/java/com/javadoterr/api/dao/UserRepositoy.java
@@ -1,11 +1,16 @@
 package com.javadoterr.api.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.javadoterr.api.entity.User;
 
 @Repository
 public interface UserRepositoy extends JpaRepository<User, Integer>{
+	
+	@Query("select u from User u where u.email = :email")
+	public User getUserByUserName(@Param("email") String email);
 
 }
