12299b5c4ae2d2e699ee485ab32470ba08cf61e5
javadoterr <javadoterr@gmail.com>
2023-04-26 17:26:45 +0530

    getting user in UserController

--- smartcontactmanager/src/main/java/com/javadoterr/api/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/javadoterr/api/controller/UserController.java
@@ -1,16 +1,34 @@
 package com.javadoterr.api.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.javadoterr.api.dao.UserRepositoy;
+import com.javadoterr.api.entity.User;
 
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
 	
+	@Autowired
+	private UserRepositoy userRepositoy;
+	
 	@GetMapping(path = "/index")
-	public String dashboard() {
+	public String dashboard(Model model, Principal principal) {
 		
+		String username = principal.getName();
+		
+		//get the user using username(email)
+		User user = userRepositoy.getUserByUserName(username);
+		
+		System.out.println("USER - "+ user);
+		
+		model.addAttribute("user", user);		
 		
 		return "normal/user_dashboard";
 	}
