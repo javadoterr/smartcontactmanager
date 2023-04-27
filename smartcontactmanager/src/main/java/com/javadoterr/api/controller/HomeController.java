d3a940fcd23f1bf2ef0086d464cac2e851c4990a
javadoterr <javadoterr@gmail.com>
2023-04-27 13:08:22 +0530

    Designing Add Contact Form

--- smartcontactmanager/src/main/java/com/javadoterr/api/controller/HomeController.java
+++ smartcontactmanager/src/main/java/com/javadoterr/api/controller/HomeController.java
@@ -18,73 +18,71 @@ import jakarta.validation.Valid;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	private BCryptPasswordEncoder encoder;
-	
+
 	@Autowired
 	private UserRepositoy userRepositoy;
-	
+
 	@GetMapping(path = "/")
 	public String home(Model model) {
 		model.addAttribute("title", "Home - Smart Contact Manager");
 		return "home";
 	}
-	
+
 	@GetMapping(path = "/about")
 	public String about(Model model) {
 		model.addAttribute("title", "About - Smart Contact Manager");
 		return "about";
 	}
-	
+
 	@GetMapping(path = "/signup")
 	public String signup(Model model) {
 		model.addAttribute("title", "Register - Smart Contact Manager");
 		model.addAttribute("user", new User());
 		return "signup";
 	}
-	
+
 //	handler for registering user
 	@PostMapping(path = "/do_register")
-	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result,
-										Model model, HttpSession session) {
-		
-		
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, Model model,
+			HttpSession session) {
+
 		try {
-			
-			//agreement is not working
-			/* 
+
+			// agreement is not working
+			/*
 			 * if(!agreement) {
 			 * System.out.println("You have not agreed the terms and conditions !"); throw
 			 * new Exception("You have not agreed the terms and conditions !"); }
 			 */
-			if(result.hasErrors()) {
-				result.getAllErrors().forEach(err -> System.out.println("ERROR -> "+ err.getDefaultMessage()));
+			if (result.hasErrors()) {
+				result.getAllErrors().forEach(err -> System.out.println("ERROR -> " + err.getDefaultMessage()));
 				model.addAttribute("user", user);
 				return "signup";
 			}
-			
+
 			user.setPassword(encoder.encode(user.getPassword()));
 			user.setRole("ROLE_USER");
 			user.setEnables(true);
 			user.setImageUrl("default.jpg");
-			
+
 			User savedUser = userRepositoy.save(user);
-			
-			
-			
+
 			model.addAttribute("user", new User());
+			model.addAttribute("title", "Signup");
 			session.setAttribute("message", new Message("Successfully Registered !", "alert-success"));
 		} catch (Exception e) {
 			e.printStackTrace();
 			model.addAttribute("user", user);
-			session.setAttribute("message", new Message("Something went worng !"+e.getMessage(), "alert-danger"));
+			session.setAttribute("message", new Message("Something went worng !" + e.getMessage(), "alert-danger"));
 			return "signup";
 		}
 		return "signup";
 	}
 
-	//handler for custom Login
+	// handler for custom Login
 	@GetMapping(path = "/signin")
 	public String customLogin(Model model) {
 		model.addAttribute("title", "Login Page - Smart Contact Manager");
--- smartcontactmanager/src/main/java/com/javadoterr/api/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/javadoterr/api/controller/UserController.java
@@ -6,31 +6,53 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.javadoterr.api.dao.UserRepositoy;
+import com.javadoterr.api.entity.Contact;
 import com.javadoterr.api.entity.User;
 
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserRepositoy userRepositoy;
+
 	
+	//this method execute for every end point of method to add data in model
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
+
+		String username = principal.getName();
+
+		// get the user using username(email)
+		User user = userRepositoy.getUserByUserName(username);
+
+		System.out.println("USER - " + user);
+
+		model.addAttribute("user", user);
+
+	}
+
+	//dashboard home
 	@GetMapping(path = "/index")
 	public String dashboard(Model model, Principal principal) {
 		
-		String username = principal.getName();
-		
-		//get the user using username(email)
-		User user = userRepositoy.getUserByUserName(username);
-		
-		System.out.println("USER - "+ user);
-		
-		model.addAttribute("user", user);		
-		
+		model.addAttribute("title", "User Dashboard");
+
 		return "normal/user_dashboard";
+	}
+
+	// open add form controller
+	@GetMapping(path = "/add-contact")
+	public String openAddContactForm(Model model) {
+
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+
+		return "normal/add_contact_form";
 	}
 
 }
